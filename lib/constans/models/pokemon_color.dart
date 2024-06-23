37553c2c096c186478bd87666ce20ba429c1a31b
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-06-23 12:57:23 +0200

    level matching for types

---
+++ lib/constans/models/pokemon_color.dart
@@ -0,0 +1,22 @@
+   import 'package:flutter/material.dart';
+
+final Map<String, Color> PokeColors = {
+  'normal': Colors.grey,
+  'fire': Colors.red,
+  'water': Colors.blue,
+  'electric': Colors.yellow,
+  'grass': Colors.green,
+  'ice': Colors.lightBlue,
+  'fighting': Colors.orange,
+  'poison': Colors.purple,
+  'ground': Colors.brown,
+  'flying': Colors.lightBlueAccent,
+  'psychic': Colors.pink,
+  'bug': Colors.lightGreen,
+  'rock': Colors.brown,
+  'ghost': Colors.purpleAccent,
+  'dragon': Colors.indigo,
+  'dark': const Color.fromARGB(255, 73, 73, 73),
+  'steel': Colors.blueGrey,
+  'fairy': Colors.pinkAccent,
+};
