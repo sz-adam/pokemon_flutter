3f6e26ba1d65dabab1fdc021cf184ff27f541935
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-06-23 21:29:10 +0200

    map function and text style

--- lib/widget/type.dart
+++ lib/widget/type.dart
@@ -1,36 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pokemon/constans/models/pokemon_color.dart';
 import 'package:flutter_pokemon/constans/models/pokemon_details_model.dart';
 
 class Type extends StatelessWidget {
   const Type({Key? key, required this.pokemonDetail}) : super(key: key);
   final PokemonDetails pokemonDetail;
+
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        if (pokemonDetail.types.length > 0)
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 4),
-            decoration: BoxDecoration(
-                border: Border.all(width: 1, color: Colors.black),
-                borderRadius: BorderRadius.circular(16)),
-            child: Text(
-              pokemonDetail.types[0],
-            ),
+      children: pokemonDetail.types.map((type) {
+        Color backgroundColor = PokeColors[type.toLowerCase()] ?? Colors.grey;
+        return Container(
+          padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 8),
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            border: Border.all(width: 1, color: Colors.black),
+            borderRadius: BorderRadius.circular(20),
           ),
-        if (pokemonDetail.types.length > 1)
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 4),
-            decoration: BoxDecoration(
-              border: Border.all(width: 1, color: Colors.black),
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Text(
-              pokemonDetail.types[1],
-            ),
+          child: Text(
+            type,
+            style: const TextStyle(
+                color: Colors.white, fontWeight: FontWeight.w600, fontSize: 15),
           ),
-      ],
+        );
+      }).toList(),
     );
   }
 }
