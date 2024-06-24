dd7f7c12fa2a2f567a63ba7ae836b65183dec928
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-06-24 23:50:17 +0200

    stat names are capitalized

--- lib/constans/models/pokemon_details_model.dart
+++ lib/constans/models/pokemon_details_model.dart
@@ -21,39 +21,39 @@ class PokemonDetails {
     required this.stats,
   });
 
-factory PokemonDetails.fromJson(Map<String, dynamic> json) {
-  List<dynamic> typesList = json['types'];
-  List<String> types =
-      typesList.map((type) => _capitalize(type['type']['name'] as String)).toList();
+  factory PokemonDetails.fromJson(Map<String, dynamic> json) {
+    List<dynamic> typesList = json['types'];
+    List<String> types = typesList
+        .map((type) => _capitalize(type['type']['name'] as String))
+        .toList();
 
-  List<dynamic> abilitiesList = json['abilities'];
-  List<Map<String, dynamic>> abilities = abilitiesList
-      .map((ability) => ability['ability'])
-      .toList().cast<Map<String, dynamic>>();
+    List<dynamic> abilitiesList = json['abilities'];
+    List<Map<String, dynamic>> abilities = abilitiesList
+        .map((ability) => ability['ability'])
+        .toList()
+        .cast<Map<String, dynamic>>();
 
-  List<dynamic> statsList = json['stats'];
-  List<Map<String, dynamic>> stats = statsList
-      .map((stat) => {
-            'name': stat['stat']['name'],
-            'base_stat': stat['base_stat'],
-          })
-      .toList().cast<Map<String, dynamic>>();
+    List<dynamic> statsList = json['stats'];
+    List<Map<String, dynamic>> stats = statsList
+        .map((stat) => {
+              'name': _capitalize(stat['stat']['name']),
+              'base_stat': stat['base_stat'],
+            })
+        .toList()
+        .cast<Map<String, dynamic>>();
 
- 
-
-  return PokemonDetails(
-    name: _capitalize(json['name']),
-    imageUrl: json['sprites']['other']['home']['front_default'],
-    id: json['id'],
-    types: types,
-    weight: json['weight'],
-    height: json['height'],
-    baseExperience: json['base_experience'],
-    abilities: abilities,
-    stats: stats,
-  );
-}
-
+    return PokemonDetails(
+      name: _capitalize(json['name']),
+      imageUrl: json['sprites']['other']['home']['front_default'],
+      id: json['id'],
+      types: types,
+      weight: json['weight'],
+      height: json['height'],
+      baseExperience: json['base_experience'],
+      abilities: abilities,
+      stats: stats,
+    );
+  }
 
   static String _capitalize(String name) {
     if (name.isEmpty) return name;
