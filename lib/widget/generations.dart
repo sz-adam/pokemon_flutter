8e6a2d808ba001a3fe12323795862649eba51170
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-08-14 20:02:50 +0200

    amendment listView  -> GriedView

--- lib/widget/generations.dart
+++ lib/widget/generations.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pokemon/constants/models/generation.dart';
 import 'package:flutter_pokemon/constants/services/poke_service.dart';
+import 'package:flutter_pokemon/constants/utils/caracter_format.dart';
 
 class Generations extends StatefulWidget {
   @override
@@ -17,30 +18,47 @@ class _GenerationsState extends State<Generations> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {   
     return Scaffold(
        backgroundColor: Theme.of(context).colorScheme.onSurfaceVariant,
       appBar: AppBar(
          backgroundColor: Theme.of(context).colorScheme.primary,
-        title: const Center(child: Text('Pokemon Generations')),
+        title: const Center(child: Text('Pokemon Generations',style: TextStyle(color: Colors.white),)),
       ),
-      body: FutureBuilder<List<Generation>>(
+ body: FutureBuilder<List<Generation>>(
         future: _generations,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
+            // Betöltésjelző megjelenítése az adatok lekérése közben
+            return  const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
+            // Hiba esetén hibaüzenet megjelenítése
             return Center(child: Text('Error: ${snapshot.error}'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No generations found'));
+            // Ha nincsenek generációk
+            return  const Center(child: Text('No generations found'));
           } else {
+            // adatok megjelenítése
             final generations = snapshot.data!;
-            return ListView.builder(
+            return GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // Két kártya egymás mellé
+                crossAxisSpacing: 8, // Vízszintes térköz a kártyák között
+                mainAxisSpacing: 8, // Függőleges térköz a kártyák között
+                childAspectRatio: 1.5, // Kártyák méretaránya
+              ),
               itemCount: generations.length,
               itemBuilder: (context, index) {
                 final generation = generations[index];
-                return ListTile(
-                  title: Text(generation.name),
+                final formattedName=formatGenerationName(generation.name);
+                return Card(    
+                    child: 
+                      Center(
+                        child: Text(
+                          formattedName,
+                          style:const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                        ),
+                      ),
                 );
               },
             );
