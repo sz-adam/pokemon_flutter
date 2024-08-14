b9af5a246638dd4a722c5da6800a4c96627e5c7e
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-08-14 20:03:30 +0200

    character formatting first letter uppercase - after all uppercase

---
+++ lib/constants/utils/caracter_format.dart
@@ -0,0 +1,18 @@
+String formatGenerationName(String name) {
+  if (name.isEmpty) return name;
+
+  // kötőjel alapján felosztja a szöveget 
+  final parts = name.split('-');
+
+  // első rész formázása kezdő betű nagybetű 
+  final firstPart = parts[0];
+  final formattedFirstPart = firstPart[0].toUpperCase() + firstPart.substring(1).toLowerCase();
+
+  // kötöjel utáni összes betű nagybetű 
+  final formattedRestParts = parts.sublist(1).map((part) {
+    return part.toUpperCase();
+  }).toList();
+
+  // Csatlakoztatjuk a részeket
+  return [formattedFirstPart, ...formattedRestParts].join('-');
+}
