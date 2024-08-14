8fde1a1a4b06a24a9a3bfd6b0c4427b491f1b0eb
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-08-14 20:03:46 +0200

    format

--- lib/widget/generations.dart
+++ lib/widget/generations.dart
@@ -18,25 +18,29 @@ class _GenerationsState extends State<Generations> {
   }
 
   @override
-  Widget build(BuildContext context) {   
+  Widget build(BuildContext context) {
     return Scaffold(
-       backgroundColor: Theme.of(context).colorScheme.onSurfaceVariant,
+      backgroundColor: Theme.of(context).colorScheme.onSurfaceVariant,
       appBar: AppBar(
-         backgroundColor: Theme.of(context).colorScheme.primary,
-        title: const Center(child: Text('Pokemon Generations',style: TextStyle(color: Colors.white),)),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: const Center(
+            child: Text(
+          'Pokemon Generations',
+          style: TextStyle(color: Colors.white),
+        )),
       ),
- body: FutureBuilder<List<Generation>>(
+      body: FutureBuilder<List<Generation>>(
         future: _generations,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             // Betöltésjelző megjelenítése az adatok lekérése közben
-            return  const Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
             // Hiba esetén hibaüzenet megjelenítése
             return Center(child: Text('Error: ${snapshot.error}'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
             // Ha nincsenek generációk
-            return  const Center(child: Text('No generations found'));
+            return const Center(child: Text('No generations found'));
           } else {
             // adatok megjelenítése
             final generations = snapshot.data!;
@@ -50,15 +54,15 @@ class _GenerationsState extends State<Generations> {
               itemCount: generations.length,
               itemBuilder: (context, index) {
                 final generation = generations[index];
-                final formattedName=formatGenerationName(generation.name);
-                return Card(    
-                    child: 
-                      Center(
-                        child: Text(
-                          formattedName,
-                          style:const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                        ),
-                      ),
+                final formattedName = formatGenerationName(generation.name);
+                return Card(
+                  child: Center(
+                    child: Text(
+                      formattedName,
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.w600),
+                    ),
+                  ),
                 );
               },
             );
