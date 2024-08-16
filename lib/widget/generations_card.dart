b4e00ec45e67eb70bb71a542fc3660fad00f6ca5
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-08-16 21:01:17 +0200

    repair name

--- lib/widget/generations_card.dart
+++ lib/widget/generations_card.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_pokemon/widget/generation_details.dart';
+import 'package:flutter_pokemon/widget/generation_pokemons.dart';
 
 class GenerationsCard extends StatelessWidget {
   const GenerationsCard({
